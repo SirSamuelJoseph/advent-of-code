620bffcf0918c185315d27cdfc0094a7840ab801
Samuel Berin <samuel.berin@hqo.co>
2021-12-03 09:50:31 -0700

    Commenting

--- 2021/3/main.py
+++ 2021/3/main.py
@@ -1,17 +1,20 @@
-
+# Gets the power consumption for the given binaries
 def getPowerConsumption(binaries):
     bitLength = len(binaries[0]) - 1 # subtract the newline
     mostCommon = [0] * bitLength
     for i in range(bitLength):
         mostCommon[i] = getMostCommonBitFromPosition(binaries, i)
+    # Invert the list to find the least common bit for each position
     leastCommonString = ''.join(['1' if i == '0' else '0' for i in mostCommon])
     mostCommonString = ''.join(mostCommon)
+    # Convert from binary to base 10
     gamma = int(mostCommonString, 2)
     epsilon = int(leastCommonString, 2)
     product = gamma * epsilon
     returnString = "Gamma: {}, Epsilon: {}, Product: {}"
     print(returnString.format(gamma, epsilon, product))
 
+# Gets the life support rating for the given binaries
 def getLifeSupportRating(binaries):
     oxygen = int(getLifeSupportSubsystem(binaries, True), 2)
     co2 = int(getLifeSupportSubsystem(binaries, False), 2)
@@ -19,29 +22,36 @@ def getLifeSupportRating(binaries):
     returnString = "Oxygen: {}, CO2: {}, Product: {}"
     print(returnString.format(oxygen, co2, product))
 
+# Handles a single life support subsystem (Oxygen or CO2)
 def getLifeSupportSubsystem(binaries, isOxygen):
     contenders =  binaries
     index = 0
     while(len(contenders) > 1):
         mostCommonNum = getMostCommonBitFromPosition(contenders, index)
+        # Stay in the list if your binary string should count for this index, mostCommonNum
         contenders = [x for x in contenders if binaryCounted(x, index, mostCommonNum, isOxygen)]
         index += 1
     return contenders[0]
 
+# Import the binaries and put them into an array of strings
+# NOTE: This will keep the newlines- we'll deal with that later
 def importBinaries(path):
     file = open(path, 'r')
     binaries = file.readlines()
     return binaries
 
+# Gets the most common bit for the given position among the binaries
 def getMostCommonBitFromPosition(binaries, position):
     vals = [int(x[position], 2) for x in binaries]
     mean = float(sum(vals)) / len(binaries)
+    # Return strings here to make it easier to recombine into a single string at the end
     if mean > .5:
         return "1"
     if mean == .5:
         return "2"
     return "0"
 
+# Returns true if this binary string meets the conditions of its given subsystem
 def binaryCounted(binary, index, mostCommon, isOxygen):
     bit = binary[index]
     if isOxygen:
@@ -55,5 +65,5 @@ def binaryCounted(binary, index, mostCommon, isOxygen):
         else:
             return bit != mostCommon
 
-getPowerConsumption(importBinaries("input.txt"))
+getPowerConsumption(importBinaries())
 getLifeSupportRating(importBinaries("input.txt"))
