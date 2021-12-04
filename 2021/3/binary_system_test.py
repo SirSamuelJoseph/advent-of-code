471e4af422ca7011144fe405eaa25056860b161a
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 09:32:11 -0700

    right import

--- 2021/3/binary_system_test.py
+++ 2021/3/binary_system_test.py
@@ -2,7 +2,7 @@ import unittest
 import binary_system
 
 class TestGetBinarySystemResults(unittest.TestCase):
-    testInput = [line.rstrip() for line in open("2021/2/testinput.txt")]
+    testInput = [line.rstrip() for line in open("2021/3/testinput.txt")]
     def test_get_power_consumption(self):
         val = binary_system.getPowerConsumption(self.testInput)
         self.assertEqual(val, 198, "Wrong Value")
