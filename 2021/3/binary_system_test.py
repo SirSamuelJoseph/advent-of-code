d7cab57174672807acf32fdd1f8a06a348ffe5b2
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 09:30:43 -0700

    update test input

--- 2021/3/binary_system_test.py
+++ 2021/3/binary_system_test.py
@@ -4,9 +4,9 @@ import binary_system
 class TestGetBinarySystemResults(unittest.TestCase):
     testInput = [line.rstrip() for line in open("2021/2/testinput.txt")]
     def test_get_power_consumption(self):
-        val = binary_system.getPowerConsumption(testInput)
+        val = binary_system.getPowerConsumption(self.testInput)
         self.assertEqual(val, 198, "Wrong Value")
     
     def test_get_life_support_rating(self):
-        val = binary_system.getLifeSupportRating(binaries)
+        val = binary_system.getLifeSupportRating(self.testInput)
         self.assertEqual(val, 230, "Wrong Value")
