f51b809c48b69da01d9b4147f886216a0e44c71b
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 09:14:43 -0700

    rename problem variable

--- 2021/2/position_test.py
+++ 2021/2/position_test.py
@@ -5,8 +5,8 @@ class TestGetPosition(unittest.TestCase):
     testInput = [line.rstrip() for line in open("2021/2/testinput.txt")]
     def test_get_position(self):
         pos = position.getPositionFromDirections(self.testInput)
-        self.assertEqual(count, 150, "Wrong Place")
+        self.assertEqual(pos, 150, "Wrong Place")
     
     def test_get_complex_position(self):
         pos = position.getComplexPositionFromDirections(self.testInput)
-        self.assertEqual(count, 5, "Wrong Place")
+        self.assertEqual(pos, 5, "Wrong Place")
