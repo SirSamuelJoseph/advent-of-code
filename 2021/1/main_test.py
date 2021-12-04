80882b71937442347e715afbe2f9513bf302a191
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 07:37:26 -0700

    unneeded python test code

--- 2021/1/main_test.py
+++ 2021/1/main_test.py
@@ -10,8 +10,3 @@ class TestCountIncreases(unittest.TestCase):
     def test_count_increases_window(self):
         count = main.countIncreasesWindows(self.testInput, 3)
         self.assertEqual(count, 5, "Wrong Count")
-
-
-
-if __name__ == '__main__':
-    unittest.main()
