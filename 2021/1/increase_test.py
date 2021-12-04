8f8dd3c688cddb41369e9e470df9886baa73d009
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 09:18:14 -0700

    rename mod

--- 2021/1/increase_test.py
+++ 2021/1/increase_test.py
@@ -1,12 +1,12 @@
 import unittest
-import main
+import increase
 
 class TestCountIncreases(unittest.TestCase):
     testInput = [line.rstrip() for line in open("2021/1/testinput.txt")]
     def test_count_increases(self):
-        count = main.countIncreases(self.testInput)
+        count = increase.countIncreases(self.testInput)
         self.assertEqual(count, 7, "Wrong Count")
     
     def test_count_increases_window(self):
-        count = main.countIncreasesWindows(self.testInput, 3)
+        count = increase.countIncreasesWindows(self.testInput, 3)
         self.assertEqual(count, 5, "Wrong Count")
