1cf2484714ea3e231eef9376ed5ad4e551b5756c
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 09:11:33 -0700

    rename module

--- 2021/2/position_test.py
+++ 2021/2/position_test.py
@@ -1,12 +1,12 @@
 import unittest
-import main
+import position
 
 class TestGetPosition(unittest.TestCase):
     testInput = [line.rstrip() for line in open("2021/2/testinput.txt")]
     def test_get_position(self):
-        pos = main.getPositionFromDirections(testInput)
+        pos = position.getPositionFromDirections(testInput)
         self.assertEqual(count, 150, "Wrong Place")
     
     def test_get_complex_position(self):
-        pos = main.getComplexPositionFromDirections(testInput)
+        pos = position.getComplexPositionFromDirections(testInput)
         self.assertEqual(count, 5, "Wrong Place")
