2b94fa19ced4abf90c6247555d21d6aabcc9387d
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 05:59:06 -0700

    Rewrite tests

--- 2021/1/main_test.go
+++ 2021/1/main_test.go
@@ -6,17 +6,52 @@ import (
 )
 
 func TestCountIncreases(t *testing.T) {
-	sampleInput := importNumsFromFile("testinput.txt")
-	increases := countIncreases(sampleInput)
-	if increases != 7 {
-		t.Errorf("Incorrect Increase Count; got %d, wanted %d", increases, 7)
+
+	tests := map[string]struct {
+		input     []int
+		increases int
+	}{
+		"testinput": {
+			input:     importNumsFromFile("testinput.txt"),
+			increases: 7,
+		},
+		"emptyInput": {
+			input:     []int{},
+			increases: 0,
+		},
+	}
+
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			count := countIncreases(testcase.input)
+			if count != testcase.increases {
+				t.Errorf("Incorrect Increase Count; got %d, wanted %d", count, testcase.increases)
+			}
+		})
 	}
 }
 
 func TestCountWindowIncreases(t *testing.T) {
-	sampleInput := importNumsFromFile("testinput.txt")
-	increases := countIncreasesWindow(sampleInput, math.MaxInt32)
-	if increases != 5 {
-		t.Errorf("Incorrect Increase Count; got %d, wanted %d", increases, 5)
+	tests := map[string]struct {
+		input     []int
+		increases int
+	}{
+		"testinput": {
+			input:     importNumsFromFile("testinput.txt"),
+			increases: 5,
+		},
+		"emptyInput": {
+			input:     []int{},
+			increases: 0,
+		},
+	}
+
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			count := countIncreasesWindow(testcase.input, math.MaxInt32)
+			if count != testcase.increases {
+				t.Errorf("Incorrect Increase Count; got %d, wanted %d", count, testcase.increases)
+			}
+		})
 	}
 }
