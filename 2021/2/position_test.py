1372cdca77df3e69927c01f898874baa2bfa37a7
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-04 09:13:39 -0700

    Change input name

--- 2021/2/position_test.py
+++ 2021/2/position_test.py
@@ -4,9 +4,9 @@ import position
 class TestGetPosition(unittest.TestCase):
     testInput = [line.rstrip() for line in open("2021/2/testinput.txt")]
     def test_get_position(self):
-        pos = position.getPositionFromDirections(testInput)
+        pos = position.getPositionFromDirections(self.testInput)
         self.assertEqual(count, 150, "Wrong Place")
     
     def test_get_complex_position(self):
-        pos = position.getComplexPositionFromDirections(testInput)
+        pos = position.getComplexPositionFromDirections(self.testInput)
         self.assertEqual(count, 5, "Wrong Place")
