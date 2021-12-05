ce54796be9065f2335f3b8a05876ebf2394e3aa1
SirSamuelJoseph <samuel.servicerouter+GH@gmail.com>
2021-12-05 15:35:16 -0700

    Fix nondefaults

--- 2021/5/vents_test.py
+++ 2021/5/vents_test.py
@@ -4,9 +4,9 @@ import vents
 class TestGetBinarySystemResults(unittest.TestCase):
     testInput = [line.rstrip() for line in open("2021/5/testinput.txt")]
     def test_get_vents_count(self):
-        count = vents.getIntersectionsAboveOne(self.testInput, False)
+        count = vents.getIntersectionsAboveOne(self.testInput, False, {})
         self.assertEqual(count, 5, "Wrong Value")
     
     def test_get_vents_count_diagonal(self):
-        count = vents.getIntersectionsAboveOne(self.testInput, True)
+        count = vents.getIntersectionsAboveOne(self.testInput, True, {})
         self.assertEqual(count, 12, "Wrong Value")
