12683817685ada87403780eb94cf2e68130af5af
Samuel Berin <samuel.berin@hqo.co>
2021-12-06 08:14:29 -0700

    Day 6 innit

--- 2021/6/lanternfish_test.py
+++ 2021/6/lanternfish_test.py
@@ -2,7 +2,7 @@ import unittest
 import lanternfish
 
 class TestGetLanternfishCount(unittest.TestCase):
-    testInput = [line.rstrip() for line in open("2021/5/testinput.txt")]
+    testInput = [line.rstrip() for line in open("2021/6/testinput.txt")]
     def test_get_short_count(self):
         start = lanternfish.initState(self.testInput)
         count = lanternfish.advanceGivenDays(start, 80)
