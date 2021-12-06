eca3015e23d1d8361ffe4340270c8f3b7d423862
Samuel Berin <samuel.berin@hqo.co>
2021-12-06 08:11:59 -0700

    Remove cruft and add tests

--- 2021/6/lanternfish.py
+++ 2021/6/lanternfish.py
@@ -1,24 +1,4 @@
 def advanceDay(state):
-    newFish = []
-    fishToAdd = 0
-    for fish in state:
-        num = int(fish)
-        if num > 0:
-            newFish.append(num - 1)
-        elif num == 0:
-            newFish.append(6)
-            fishToAdd += 1
-    for i in range(fishToAdd):
-        newFish.append(8)
-    return newFish
-
-def advanceDay2(state):
-    newFish = [int(x) - 1 if int(x) > 0 else 6 for x in state]
-    for i in range(state.count(0)):
-        newFish.append(8)
-    return newFish
-
-def advanceDay3(state):
     oldState = list(state)
     for i in range(9):
         state[8 - i] = oldState[9 - i]
@@ -29,16 +9,21 @@ def advanceDay3(state):
 
 def advanceGivenDays(state, days):
     for i in range(days):
-        state =  advanceDay3(state)
+        state =  advanceDay(state)
         print("Day {}: {} Fish".format(i + 1, sum(state)))
+    return sum(state)
 
-def main():
-    inputs = [line.rstrip() for line in open("input.txt")]
+def initState(inputs):
     initial = inputs[0].split(',')
     counts = [0] * 10
     for num in initial:
         counts[int(num)] += 1
-    advanceGivenDays(counts, 80)
+    return counts
+
+def main():
+    inputs = [line.rstrip() for line in open("input.txt")]
+    counts = initState(inputs)
+    advanceGivenDays(counts, 256)
 
 if __name__ == "__main__":
     main()
